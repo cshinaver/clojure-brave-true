3f6270db1be9fb1ce3a115522ab134f18e53750c
cshinaver <bshinaver@gmail.com>
2017-01-25 13:42:43 -0500

    added variable arity function

--- clojure-noob/src/clojure_noob/core.clj
+++ clojure-noob/src/clojure_noob/core.clj
@@ -7,8 +7,17 @@
    (println (str "I " chop " chop " name "!")))
   ([name] (x-chop name "karate")))
 
+(defn x-attack
+  "Test variable arity function"
+  [name & attacks]
+  (println
+   (str "I "
+        (clojure.string/join ", " (drop-last attacks)) " and " (last attacks) " " name)
+   ))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (x-chop "kanye" "ninja")
-  (x-chop "jim"))
+  (x-chop "jim")
+  (x-attack "jim" "hit" "kick" "slap" "poke"))
