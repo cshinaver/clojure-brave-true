5da3814b9587c5f71379fe64ba87ed7c148fc26b
cshinaver <bshinaver@gmail.com>
2017-01-25 18:14:17 -0500

    added hobbit body parts

--- clojure-noob/src/clojure_noob/core.clj
+++ clojure-noob/src/clojure_noob/core.clj
@@ -3,12 +3,13 @@
   (:gen-class))
 
 (defn -main
- "I don't do a whole lot ... yet."
- [& args]
- (x-chop "kanye" "ninja")
- (x-chop "jim")
- (x-attack "jim" "hit" "kick" "slap" "poke")
- (chooser ["herp" "derp" "merp" "slerp"])
- (announce-treasure-location {:lat 9.9 :lng 91.231})
- (announce-gun-ratio {:butter 50 :guns 2})
- (print-special-sum))
+  "I don't do a whole lot ... yet."
+  [& args]
+  (x-chop "kanye" "ninja")
+  (x-chop "jim")
+  (x-attack "jim" "hit" "kick" "slap" "poke")
+  (chooser ["herp" "derp" "merp" "slerp"])
+  (announce-treasure-location {:lat 9.9 :lng 91.231})
+  (announce-gun-ratio {:butter 50 :guns 2})
+  (print-special-sum)
+  (println (better-symmetrize-body-parts asym-hobbit-body-parts)))
--- clojure-noob/src/clojure_noob/hobbit.clj
+++ clojure-noob/src/clojure_noob/hobbit.clj
@@ -1,1 +1,50 @@
 (ns clojure-noob.hobbit)
+
+(def asym-hobbit-body-parts
+  [{:name "head" :size 3}
+   {:name "left-eye" :size 1}
+   {:name "left-ear" :size 1}
+   {:name "mouth" :size 1}
+   {:name "nose" :size 1}
+   {:name "neck" :size 2}
+   {:name "left-shoulder" :size 3}
+   {:name "left-upper-arm" :size 3}
+   {:name "chest" :size 10}
+   {:name "back" :size 10}
+   {:name "left-forearm" :size 3}
+   {:name "abdomen" :size 6}
+   {:name "left-kidney" :size 1}
+   {:name "left-hand" :size 2}
+   {:name "left-knee" :size 2}
+   {:name "left-thigh" :size 4}
+   {:name "left-lower-leg" :size 3}
+   {:name "left-achilles" :size 1}
+   {:name "left-foot" :size 2}])
+
+
+(defn matching-part
+  [part]
+  {:name (clojure.string/replace (:name part) #"^left-" "right-")
+   :size (:size part)})
+
+(defn symmetrize-body-parts
+  "Seq of maps with :name and :size"
+  [asym-body-parts]
+  (loop [remaining-asym-parts asym-body-parts
+         final-asym-parts []]
+    (if (empty? remaining-asym-parts)
+      final-asym-parts
+      (let [[part & remaining] remaining-asym-parts]
+        (recur
+         remaining
+         (into final-asym-parts
+               (set [part (matching-part part)]))))))) 
+
+(defn better-symmetrize-body-parts
+  [asym-body-parts]
+  (reduce
+   (fn [a part]
+     (into a (set [part (matching-part part)]))
+     )
+   []
+   asym-body-parts))
