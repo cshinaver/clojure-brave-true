6eaef13567bfc43ede225d80722b90c9fbd3715c
cshinaver <bshinaver@gmail.com>
2017-01-25 13:36:02 -0500

    add multi arity example

--- clojure-noob/src/clojure_noob/core.clj
+++ clojure-noob/src/clojure_noob/core.clj
@@ -1,7 +1,14 @@
 (ns clojure-noob.core
   (:gen-class))
 
+(defn x-chop
+  "Test multi arity fuction"
+  ([name chop]
+   (println (str "I " chop " chop " name "!")))
+  ([name] (x-chop name "karate")))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (x-chop "kanye" "ninja")
+  (x-chop "jim"))
