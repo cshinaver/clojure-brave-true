02913e1711311dd7dd222ae67623e8b2d4308231
cshinaver <bshinaver@gmail.com>
2017-02-28 00:53:14 -0500

    added enlive an cljhttp as dependencies

--- project.clj
+++ project.clj
@@ -3,7 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [clj-http "2.3.0"]
+                 [enlive "1.1.6"]]
   :main ^:skip-aot clojure-noob.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
